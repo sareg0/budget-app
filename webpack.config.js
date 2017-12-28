3276542d3bb999d090db1b7a33ec0562747a2f25
Sara Regan <regan.sara@gmail.com>
2017-12-28 21:29:10 +0100

    startup budget app

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+  entry: './src/entry.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader'}
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+}
